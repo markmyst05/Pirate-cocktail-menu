cc6eb20f5e6e36124bec2422b9a5fb839f52f1ac
Mark Mills <44096526-markmyst05@users.noreply.replit.com>
2025-06-26 21:11:58 +0000

    Initial commit with GitHub Actions deploy setup

--- .vite/deps/chunk-EZSDZQRT.js
+++ .vite/deps/chunk-EZSDZQRT.js
@@ -1,4 +1,4 @@
-var __getOwnPropNames = Object.getOwnPropertyNames;
+evar __getOwnPropNames = Object.getOwnPropertyNames;
 var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
-  base: '/Pirate-cocktail-menu/', // match your GitHub repo name
+  base: '/pirate-cocktail-menu/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+  define: {
+    'process.env': {},
+    global: 'window',
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
 });
